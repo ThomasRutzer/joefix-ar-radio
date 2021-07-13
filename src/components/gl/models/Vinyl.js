65648cff53571500815c87504a7665275bf551c8
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-13 22:30:34 +0200

    Use JSX graph for Vinyl

--- src/components/gl/models/Vinyl.js
+++ src/components/gl/models/Vinyl.js
@@ -1,15 +1,15 @@
 import { useGLTF } from "@react-three/drei"
 import { animated } from "@react-spring/three"
 import { useEffect, useRef } from "react"
+import { useFrame } from "@react-three/fiber"
 
 import vinylGLTF from "./../../../assets/gltf/vinyl.glb"
 import useStore from "../../../store"
 import { APP_STATES } from "../../../config"
-import { useFrame } from "@react-three/fiber"
 
 const Vinyl = (props) => {
   const groupRef = useRef()
-  const { scene } = useGLTF(vinylGLTF)
+  const { nodes, materials } = useGLTF(vinylGLTF)
   const appStateRef = useRef()
 
   useEffect(() => useStore.subscribe(
@@ -24,17 +24,40 @@ const Vinyl = (props) => {
   })
 
   return (
-    <animated.group
-      {...props}
-      ref={groupRef}
-    >
-      <primitive
+    <animated.group {...props} ref={groupRef}>
+      <group
+        scale={[1.9, 1.9, 1.9]}
+        position={[0, 0.085, 0]}
+        rotation={[0, -Math.PI / 2, 0]}>
+        <group position={[0, 0, 0]}>
+          <group position={[0, 0, 0]}>
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes['buffer-0-mesh-0'].geometry}
+              material={materials._Color_009_3}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes['buffer-0-mesh-0_1'].geometry}
+              material={materials._Color_009_2}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes['buffer-0-mesh-0_2'].geometry}
+              material={materials.Color_M00}
+            />
+          </group>
+        </group>
+      </group>
+      {/* <primitive
         object={scene}
         scale={[1.9, 1.9, 1.9]}
         position={[0, 0.085, 0]}
         rotation={[-Math.PI / 2, 0, 0]}
-
-      />
+      /> */}
     </animated.group >
   )
 }
