a049903f5eb3846620890a962f552fcc2633cccb
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-13 22:30:51 +0200

    Re-use materials of Turntable

--- src/components/gl/models/Turntable.js
+++ src/components/gl/models/Turntable.js
@@ -1,19 +1,19 @@
 import { useGLTF, useTexture } from "@react-three/drei"
+import { useMemo } from "react"
+import { MeshMatcapMaterial } from "three"
 
 import matcapAssetBlack from "./../../../assets/images/matcap/black.png"
-import matcapAssetWhite from "./../../../assets/images/matcap/white.png"
 import turntableGLTF from "./../../../assets/gltf/turntable.gltf"
 
 const Turntable = props => {
   const matcapBlack = useTexture(matcapAssetBlack)
-  const matcapWhite = useTexture(matcapAssetWhite)
+  const meshMatcapMaterialBlack = useMemo(() => new MeshMatcapMaterial({matcap: matcapBlack}), [])
   const { nodes } = useGLTF(turntableGLTF)
 
   return (
     <group {...props}>
       <mesh
         geometry={nodes.music_player.geometry}
-        material={nodes.player_legs.material}
         castShadow
         receiveShadow>
         <meshMatcapMaterial matcap={matcapBlack} />
@@ -22,26 +22,23 @@ const Turntable = props => {
           castShadow
           receiveShadow
           position={[0, 0.05, 0]}
-          rotation={[0, 1.57, 0]}>
-          <meshMatcapMaterial matcap={matcapWhite} />
-        </mesh>
+          rotation={[0, 1.57, 0]} />
         <group position={[0, 0.07, 0]} rotation={[0, 1.57, 0]}>
           <mesh
             geometry={nodes.Circle003.geometry}
+            material={meshMatcapMaterialBlack}
             castShadow
-            receiveShadow>
-            <meshMatcapMaterial matcap={matcapWhite} />
-          </mesh>
+            receiveShadow />
           <mesh
             geometry={nodes.Circle003_1.geometry}
+            material={meshMatcapMaterialBlack}
             castShadow
             receiveShadow>
-            <meshMatcapMaterial matcap={matcapBlack} />
           </mesh>
         </group>
         <mesh
           geometry={nodes.player_legs.geometry}
-          material={nodes.player_legs.material}
+          material={meshMatcapMaterialBlack}
           castShadow
           receiveShadow
           position={[0.44, -0.07, -0.29]}
@@ -50,39 +47,35 @@ const Turntable = props => {
         />
         <mesh
           geometry={nodes.player_knob001.geometry}
+          material={meshMatcapMaterialBlack}
           castShadow
           receiveShadow
           position={[0.4, 0.05, 0.3]}
           rotation={[0, 1.57, 0]}
-          scale={[0.74, 0.74, 0.74]}>
-          <meshMatcapMaterial matcap={matcapWhite} />
-        </mesh>
+          scale={[0.74, 0.74, 0.74]} />
         <mesh
           geometry={nodes.player_knob.geometry}
+          material={meshMatcapMaterialBlack}
           castShadow
           receiveShadow
           position={[0.47, 0.05, 0.3]}
           rotation={[0, 1.57, 0]}
-          scale={[0.74, 0.74, 0.74]}>
-          <meshMatcapMaterial matcap={matcapWhite} />
-        </mesh>
+          scale={[0.74, 0.74, 0.74]} />
         <mesh
           geometry={nodes.player001.geometry}
+          material={meshMatcapMaterialBlack}
           castShadow
           receiveShadow
           position={[0.41, 0.09, -0.2]}
-          rotation={[0, 1.57, 0]}>
-          <meshMatcapMaterial matcap={matcapWhite} />
-        </mesh>
+          rotation={[0, 1.57, 0]} />
         <mesh
           geometry={nodes.player002.geometry}
+          material={meshMatcapMaterialBlack}
           castShadow
           receiveShadow
           position={[0.41, 0.03, -0.13]}
           rotation={[0, 1.57, 0]}
-          scale={[0.19, 0.06, 0.06]}>
-          <meshMatcapMaterial matcap={matcapWhite} />
-        </mesh>
+          scale={[0.19, 0.06, 0.06]} />
       </mesh>
     </group>
   )
