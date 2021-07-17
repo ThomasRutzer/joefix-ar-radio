d42eb6023d76c4c523dc0dbc5bb7d1e08b1a2694
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-17 18:36:23 +0200

    New JSX markup of App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { lazy, Suspense } from "react"
+import { Leva } from "leva"
 
 import useStore from "./store"
 import { VIEWS } from "./config"
 import Sound from "./components/sound"
 import Info from "./components/ui/info"
-import FocusHint from "./components/ui/FocusHint"
 import LoadingIndicator from "./components/ui/loadingIndicator"
 const CanvasContainer = lazy(() => import("./components/gl/canvasContainer"))
 
 const App = () => {
+
   return (
     <>
+
+      <LoadingIndicator />
+
       <Info onStartButton={() => useStore.setState({ view: VIEWS.SCENE })} />
       <>
-        {/* <FocusHint /> */}
         <Suspense fallback={<LoadingIndicator />}>
           <CanvasContainer />
         </Suspense>
       </>
 
-      {/* <Sound /> */}
+      <Sound />
+      <Leva
+        collapsed
+        hidden={process.env.NODE_ENV === "production"}
+      />
     </>
   )
 }
