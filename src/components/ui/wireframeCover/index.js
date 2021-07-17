3aed9874910074fe13d65c90cbd6fa49aa102b6a
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-17 18:34:58 +0200

    Update Wireframe Cover

--- src/components/ui/wireframeCover/index.js
+++ src/components/ui/wireframeCover/index.js
@@ -1,9 +1,9 @@
-import asset from "./../../../assets/images/focus-marker.png"
+import asset from "./../../../assets/images/logo-white.png"
 import "./index.css"
 
-const WireframeCover = ({ appearance, label }) => {
+const WireframeCover = ({ label }) => {
   return (
-    <div className={`wireframe-cover ${appearance === "small" ? "wireframe-cover--small" : ""}`}>
+    <div className="wireframe-cover">
       <img className="wireframe-cover__img" src={asset} alt="wireframe cover visual" aria-hidden />
       <span className="wireframe-cover__label">{label}</span>
     </div>
