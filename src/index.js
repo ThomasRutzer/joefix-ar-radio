aad5eab859d919dd573d7d4191b6434400c6075f
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-24 15:08:43 +0200

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-import "./style.css"
-import App from "./App"
-
-const reactRoot = document.getElementById("root")
-ReactDOM.render(<App />, reactRoot)
