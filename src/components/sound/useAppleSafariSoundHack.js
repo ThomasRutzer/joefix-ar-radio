c6343227600f3a8125913d2feecf03cb9995e35c
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-07-24 19:24:41 +0200

    Add hack to play audio on Safari browsers. Tired of this...

---
+++ src/components/sound/useAppleSafariSoundHack.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+
+const useAppleSafariSoundHack = soundAssets => {
+  function unlockAudio() {
+    soundAssets.map(soundAsset => {
+      console.log(soundAsset);
+      const sound = new Audio(soundAsset)
+
+      sound.play()
+      sound.pause()
+      sound.currentTime = 0
+
+      return null
+    })
+
+    document.body.removeEventListener("click", unlockAudio)
+    document.body.removeEventListener("touchstart", unlockAudio)
+  }
+
+  function isAppleDevice() {
+    const ua = navigator.userAgent.toLowerCase();
+    return ua.indexOf('safari') !== -1 && !ua.indexOf('chrome') > -1
+  }
+
+  useEffect(() => {
+    if (isAppleDevice()) {
+      document.body.addEventListener("click", unlockAudio)
+      document.body.addEventListener("touchstart", unlockAudio)
+    }
+  }, [])
+}
+
+export default useAppleSafariSoundHack
